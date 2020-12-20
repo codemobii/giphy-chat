9010c97c8b91f5db6144069d98802b50aedf83cd
John Chimaobi <45017704+ijelechimaobi@users.noreply.github.com>
2020-12-20 12:10:23 +0100

    God help me

--- client/src/App/chat.js
+++ client/src/App/chat.js
@@ -49,7 +49,9 @@ export default class Chat extends Component {
   };
 
   componentDidMount() {
-    this.socket = io("https://giphy-chat-server.vercel.app/");
+    this.socket = io("https://giphy-chat-server.vercel.app/", {
+      transports: ["websocket"],
+    });
 
     // How many users are in our socket?
     this.socket.on("users", (msg) => {
--- server/index.js
+++ server/index.js
@@ -2,13 +2,23 @@ const express = require("express");
 const app = express();
 const http = require("http").Server(app);
 const path = require("path");
+const expressStatusMonitor = require("express-status-monitor");
+
 const io = require("socket.io")(http, {
   cors: {
-    origin: ["http://localhost:3000", "https://giphy-chat.vercel.app"],
+    origin: "*",
     credentials: true,
     methods: ["GET", "POST"],
   },
 });
+
+app.use(
+  expressStatusMonitor({
+    websocket: io,
+    port: app.get("8000"),
+  })
+);
+
 const { MONGODB_URI } = require("./config");
 
 const port = process.env.PORT || 8000;
