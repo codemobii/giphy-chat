ea7545ee86247dc430ae68fd2892828e63839261
John Chimaobi <45017704+ijelechimaobi@users.noreply.github.com>
2021-01-04 00:43:42 +0100

    sever

--- client/src/App/chat.js
+++ client/src/App/chat.js
@@ -49,7 +49,7 @@ export default class Chat extends Component {
   };
 
   componentDidMount() {
-    this.socket = io("http://localhost:8000/");
+    this.socket = io("https://giphy-chat-server.vercel.app/");
 
     // How many users are in our socket?
     this.socket.on("users", (msg) => {
